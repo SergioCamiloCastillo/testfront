0a42506540ec6b3756e9b0f91cf3d9c11a6e5861
SergioCamiloCastillo <scastillonu@uniminuto.edu.co>
2022-01-12 22:56:53 -0500

    b

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react/cjs/react.development";
+import { useState, useEffect } from "react";
 import Dropdown from "./components/Dropdown";
 import "./scss/index.scss";
 import { Credentials } from "./helpers/credentials";
